24a78aa1880a92d93b107a6f36badcceb99c98af
unknown <f_alhouli_1985@hotmail.com>
2023-05-10 18:45:14 +0300

    calculation done

--- index.js
+++ index.js
@@ -8,24 +8,26 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
-  let BMI = 0 ;
-  BMI =  (weight (kg) / height^2(m));
-  console.log (BMI);
-  if 
-    (BMI <= 18.5  ) {console.log ("underwhight");
+  const hightInMeter= height/100 ;
+  const BMI = math.round (weight/ (height*height));
+  let message ;
+
+
+  if (BMI <= 18.5  ) {message ("underwhight");
+    
    }
    else if 
-   (BMI <= 24.9 ) {console.log ("Healthy Weight");
+   (BMI <= 24.9 ) {message ("Healthy Weight");
   }
    else if 
-    (BMI <=25.0  ) {console.log ("Overweight");
+    (BMI <=25.0  ) {message ("Overweight");
 }
 else if  
- (BMI <=30.0  ) {console.log ("OAbove Obesity");
+ (BMI <=30.0  ) {message ("OAbove Obesity");
 }
 else console.log ("erorr");
 
-
+ 
 
 
 
@@ -33,22 +35,22 @@ else console.log ("erorr");
 
 
 if 
-(19 <= IBM <=24  ) {console.log ("you'r age is between 19 - 24 years old","and healthy");
+(age = 19 & IBM <=24  ) {console.log ("you'r age is between 19 - 24 years old","and healthy");
 }
 else if 
-(20 <= IBM <= 25  ) {console.log ("you'r age is between 25 - 34 years old","and healthy");
+(age =20 & IBM <= 25  ) {console.log ("you'r age is between 25 - 34 years old","and healthy");
 }
 else if 
-(21 <= IBM <=26  ) {console.log ("you'r age is between 35 - 44 years old","and healthy");
+(age =21 & IBM <=26  ) {console.log ("you'r age is between 35 - 44 years old","and healthy");
 }
 else if  
-(22 <= IBM <=27  ) {console.log ("you'r age is between 45 - 54 years old","and healthy");
+(age =22 & IBM <=27  ) {console.log ("you'r age is between 45 - 54 years old","and healthy");
 }
 else if  
-(23 <= IBM <=28  ) {console.log ("you'r age is between 55 - 64 years old","and healthy");
+(age =23 & IBM <=28  ) {console.log ("you'r age is between 55 - 64 years old","and healthy");
 }
 else if  
-(24 <= IBM <=29  ) {console.log ("you'r age is between 65 and above","and healthy");
+(age =24 & IBM <=29  ) {console.log ("you'r age is between 65 and above","and healthy");
 }
 else console.log ("not healty"); 
 
